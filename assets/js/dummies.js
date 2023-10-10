e6c4ac092f1f475f60410dff91c5305260870440
Muhammad Nizar <36484593+mnizarzr@users.noreply.github.com>
2023-10-10 20:18:36 +0000

    add event page

---
+++ assets/js/dummies.js
@@ -0,0 +1,77 @@
+const events = [
+    {
+        name: "Grebek Maulud Yogyakarta",
+        location: "Yogyakarta",
+        city: "Daerah Istimewa Yogyakarta",
+        is_free: true,
+        fee: 0,
+        date: "2023-10-04",
+        thumbnail: 'grebek-maulud.png'
+    },
+    {
+        name: "Kauman Batik Art Festival",
+        location: "Kampoeng Batik Kauman",
+        city: "Kota Surakarta",
+        is_free: true,
+        fee: 0,
+        date: "2023-10-08",
+        thumbnail: 'kauman-batik-art-festival.png'
+    },
+    {
+        name: "Festival Kalipucang Wetan",
+        location: "Desa Kalipucang Wetan",
+        city: "Kabupaten Batang",
+        is_free: true,
+        fee: 0,
+        date: "2023-10-13",
+        thumbnail: 'festival-kalipucang-wetan.png'
+    },
+    {
+        name: "Pameran Flona",
+        location: "Taman Lapangan Banteng",
+        city: "Jakarta Pusat",
+        is_free: true,
+        fee: 0,
+        date: "2023-10-16",
+        thumbnail: 'pameran-flona.png'
+    },
+    {
+        name: "Pameran Garis Ombak",
+        location: "Jagad Gallery",
+        city: "Jakarta Pusat",
+        is_free: true,
+        fee: 0,
+        date: "2023-09-30",
+        thumbnail: 'pameran-garis-ombak.png'
+    },
+    {
+        name: "Attack on Titan: The Final Exhibition",
+        location: "Jl. Panjang No.5 Kebon Jeruk, Kec. Kb Jeruk",
+        city: "Kota Jakarta Barat",
+        is_free: false,
+        fee: 100000,
+        date: "2023-09-22",
+        thumbnail: 'aot-the-final-exhibition.png'
+    },
+    {
+        name: "Pameran Wisma Geha",
+        location: "Wisma Geha",
+        city: "Jakarta Pusat",
+        is_free: true,
+        fee: 0,
+        date: "2023-09-21",
+        thumbnail: 'pameran-wisma-geha.png'
+    },
+    {
+        name: "Festival Pesona Raja Ampat",
+        location: "Raja Ampat",
+        city: "Kabupaten Raja Ampat",
+        is_free: true,
+        fee: 0,
+        date: "2023-09-15",
+        thumbnail: 'festival-pesona-raja-ampat.png'
+    },
+]
+
+export const endedEvents = events.filter((event) => new Date() > new Date(event.date));
+export const upcomingEvents = events.filter((event) => new Date() < new Date(event.date));
---
+++ assets/js/event.js
@@ -0,0 +1,43 @@
+import {
+    upcomingEvents as upcomingEventsDummy,
+    endedEvents as endedEventsDummy
+} from './dummies.js';
+
+let upcomingEvents = document.getElementById('upcoming-events');
+let endedEvents = document.getElementById('ended-events');
+
+let card = (event) => {
+
+    let date = new Date(event.date);
+    let localDate = new Intl.DateTimeFormat('id-ID', {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }).format(date);
+
+    return `
+    <div class="card border-0 me-3" style="height: 380px; width: 230px; box-shadow: 0px 1px 4px 0px rgba(0,0,0,0.25);">
+        <img src="assets/images/events/${event.thumbnail}" class="card-img-top object-fit-cover" alt="${event.name}" height="150px">
+        <div class="card-body d-flex flex-column justify-content-between">
+            <p class="card-text fw-lighter" style="color: var(--light-color); font-size: 11pt">${localDate}</p>
+            <h5 class="card-title fw-semibold flex-grow-1" style="font-size: 14pt">${event.name}</h5>
+            <p class="card-text fw-lighter flex-grow-1 align-content-start" style="color: var(--light-color); font-size: 11pt">${event.location}<br>${event.city}</p>
+            <p class="card-text fw-bold fs-6">${event.is_free ? "GRATIS" : 'Rp.' + event.fee}</p>
+        </div>
+    </div>
+    `
+}
+
+upcomingEventsDummy.forEach((event) => {
+    let element = document.createElement('div');
+    element.innerHTML = card(event);
+
+    upcomingEvents.appendChild(element);
+})
+
+endedEventsDummy.forEach((event) => {
+    let element = document.createElement('div');
+    element.innerHTML = card(event);
+
+    endedEvents.appendChild(element);
+})
