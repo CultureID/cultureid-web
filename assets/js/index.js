3aff23fa6b55c8a390c5fe421279ff4676672e29
Muhammad Nizar <36484593+mnizarzr@users.noreply.github.com>
2023-10-12 07:22:21 +0000

    onscroll change navbar color

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,16 @@
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
+  const navlinks = document.querySelectorAll(".nav-link");
   if (window.scrollY > 50) {
-    navbar.classList.add("navbar-scrolled");
+    navbar.classList.add("bg-white");
+    navlinks.forEach((navlink) => {
+      navlink.style.color = "#272727"
+    })
   } else {
-    navbar.classList.remove("navbar-scrolled");
+    navbar.classList.remove("bg-white");
+    navbar.classList.add("transparent");
+    navlinks.forEach((navlink) => {
+      navlink.style.color = "white"
+    })
   }
 });
